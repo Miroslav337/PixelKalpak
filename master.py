ab8f35d88e37bcc0e3a2670fb0c8de24b9548a1f
Miroslav337 <miroslavgurenkov@gmail.com>
2026-02-08 04:10:18 +0600

    Начат код на PyQt

---
+++ master.py
@@ -0,0 +1,18 @@
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenuBar, QMenu
+import sys
+
+class Window(QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setWindowTitle('PixelKalpak')
+        self.setGeometry(300, 300, 400, 300)
+
+def start():
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
+
+if __name__ == '__main__':
+    start()
